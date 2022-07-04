51d8dcde0b180a043d504c2b6bf94afbafc25845
Gabrielscruz <54606517+Gabrielscruz@users.noreply.github.com>
2022-07-03 23:55:39 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { GetStaticProps } from 'next';
-
+import { RichText } from 'prismic-dom';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import Header from '../components/Header';
 import { getPrismicClient } from '../services/prismic';
-
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,13 +26,41 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  return (
+    <div className={styles.content}>
+      <Header />
+    </div>
+  );
+}
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const response = await prismic.getByType('publication', {
+    pageSize: 1,
+  });
+  const posts = response.results.map(post => {
+    const first_publication_date = format(
+      new Date(post.first_publication_date),
+      'dd MMM yyyy',
+      {
+        locale: ptBR,
+      }
+    );
+    return {
+      uid: post.uid,
+      first_publication_date,
+      data: {
+        title: RichText.asText(post.data.title),
+        subtitle: '',
+        author: 'Gabriel',
+      },
+    };
+  });
 
-//   // TODO
-// };
+  return {
+    props: {
+      postsPagination: { next_page: response.next_page, results: posts },
+    },
+  };
+};
--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,18 +1,8 @@
-import * as prismic from '@prismicio/client';
-import { HttpRequestLike } from '@prismicio/client';
-import { enableAutoPreviews } from '@prismicio/next';
+import * as Prismic from '@prismicio/client';
 
-export interface PrismicConfig {
-  req?: HttpRequestLike;
-}
-
-export function getPrismicClient(config: PrismicConfig): prismic.Client {
-  const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
-
-  enableAutoPreviews({
-    client,
-    req: config.req,
-  })
-
-  return client;
+export function getPrismicClient(): any {
+  const prismic = Prismic.createClient(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+  });
+  return prismic;
 }
